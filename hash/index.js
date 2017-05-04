ee83914441960c64fe9509baa61ea30015889279
ljwrer <ljwrer@sina.com>
2017-05-04 10:19:00 +0800

    linearDetection

--- hash/index.js
+++ hash/index.js
@@ -6,12 +6,27 @@
  *
  *
  */
-const hash = (str,tableSize=10e8)=>{
-    let h = 0;
+const table = new Array(26);
+const hash = (str)=>{
+    let h = new Uint32Array([0])[0];
     let i = 0;
     while (i<str.length){
         h = (h<<5)+str.charCodeAt(i++)
     }
-    return h%tableSize
+    return Math.abs(h%table.length)
 };
-
+const keys = ['acos','define','float','exp','char','atan','ceil','floor'];
+const set = function (key) {
+    const h = hash(key);
+    let i = 0;
+    // 线性探测
+    while(table[(h+i)%table.length]){
+        i++;
+        if(i>table.length){
+            throw Error('hashTable is full')
+        }
+    }
+    table[(h+i)%table.length] = key
+};
+keys.forEach(set);
+console.log(table);
